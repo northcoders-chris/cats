7b310e627e10ead0298830308e9656c87092bff3
Chris Hill <chris@northcoders.com>
2016-08-04 19:41:47 +0100

    solution

--- cats.js
+++ cats.js
@@ -1,6 +1,37 @@
-var cats = function () {
-  // your code goes here
+var catData = [
+  { id: '1', name: 'rupert', readyForHome: 'No', age: 12, personality: ['friendly', 'lazy', 'loving']},
+  { id: '2', name: 'mrs fluffy', readyForHome: 'Yes', age: 2, personality: ['affectionate', 'playful', 'shy', 'aggressive']},
+  { id: '3', name: 'tabitha', readyForHome: 'Yes', age: 4, personality: ['aggressive', 'independent']},
+  { id: '4', name: 'lily', readyForHome: 'No', age: 8, personality: ['friendly', 'playful', 'mischievous']},
+  { id: '5', name: 'stripe', readyForHome: 'Yes', age: 1, personality: ['haughty', 'independent']},
+  { id: '6', name: 'bob', readyForHome: 'Yes', age: 1, personality: ['aggressive', 'antisocial', 'nervous']},
+  { id: '7', name: 'jean claude cat damme', readyForHome: 'Yes', age: 11, personality: ['sleepy', 'shy', 'loving']},
+  { id: '8', name: 'tilly', readyForHome: 'Yes', age: 4, personality: ['playful', 'social', 'attention-seeking']},
+  { id: '9', name: 'milo', readyForHome: 'No', age: 7, personality: ['mischievous', 'friendly']},
+  { id: '10', name: 'mr claws', readyForHome: 'Yes', age: 13, personality: ['affectionate', 'shy', 'nervous']},
+  { id: '11', name: 'robert zimmercat', readyForHome: 'Yes', age: 3, personality: ['folksy', 'prolific', 'neurotic']}
+];
+
+var cats = function (cats) {
+
+  return cats.filter(function (cat) {
+    return cat.readyForHome === 'Yes' &&
+    cat.personality.indexOf('aggressive') === -1;
+  }).map(function (cat) {
+    cat.readyForHome = true;
+    cat.yearOfBirth = new Date().getFullYear() - cat.age;
+    cat.name = cat.name.split(' ').reduce(function (memo, word) {
+      return memo + capitaliseWord(word + ' ');
+    }, '').trim();
+    return cat;
+  });
+
 };
+
+function capitaliseWord(word){
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
 
 if (typeof module !== 'undefined') {
   module.exports = cats;
--- spec/cats.js
+++ spec/cats.js
@@ -11,8 +11,56 @@ describe('cats()', function () {
     expect(cats).to.be.a('function');
   });
 
-  it('does something', function () {
-    expect(true).to.equal(false);
+  it('returns an array', function () {
+    expect(cats([])).to.be.an('array');
   });
+
+  it('removes cats which are not ready to be re homes', function () {
+    var testData = [
+      { readyForHome: 'No' },
+      { readyForHome: 'Yes', name: 'chris', personality: [] }
+    ]
+    expect(cats(testData)[0].readyForHome).to.equal(true);
+  });
+
+  it('removes cats which are aggresive', function () {
+    var testData = [
+      { readyForHome: 'Yes', personality: ['aggressive'] },
+      { readyForHome: 'Yes', name: 'chris', personality: [] }
+    ]
+    expect(cats(testData)[0].personality.indexOf('aggressive')).to.equal(-1);
+  });
+
+  it('removes cats which are aggresive', function () {
+    var testData = [
+      { readyForHome: 'Yes', name: 'chris', personality: [] }
+    ]
+    expect(cats(testData)[0].readyForHome).to.equal(true);
+  });
+
+  it('removes cats which are aggresive', function () {
+    var testData = [
+      { readyForHome: 'Yes', name: 'chris', personality: [], age: 5 }
+    ]
+    var assertAge = new Date().getFullYear() - 5;
+    expect(cats(testData)[0].yearOfBirth).to.equal(assertAge);
+  });
+
+  it('capitalises the cats name', function () {
+    var testData = [
+      { readyForHome: 'Yes', personality: [], age: 5, name: 'chris' }
+    ]
+    var assertAge = new Date().getFullYear() - 5;
+    expect(cats(testData)[0].name).to.equal('Chris');
+  });
+
+  it('capitalises every word of the cats name', function () {
+    var testData = [
+      { readyForHome: 'Yes', personality: [], age: 5, name: 'mr fluffy poos' }
+    ]
+    var assertAge = new Date().getFullYear() - 5;
+    expect(cats(testData)[0].name).to.equal('Mr Fluffy Poos');
+  });
+
   // Add more assertions here
 });
